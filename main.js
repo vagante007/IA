f57de74a5cbdd16060e9a74007431d18f0b6e613
vagante007 <106813312+vagante007@users.noreply.github.com>
2024-07-29 14:27:44 +0000

    .

---
+++ main.js
@@ -0,0 +1,19 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "quem descobriu o brasil?",
+        alternativas: [
+            {
+               texto: "bolsonaro",
+               afirmação: "não foi o bolsonaro que descobrio o brasil "
+            },
+            {
+                texto: "caneta azul",
+                afirmação: "não foi o caneta azul que descobrio o brasil "
+             }
+             {
+                texto: "caneta azul",
+                afirmação: "não foi o caneta azul que descobrio o brasil "
+             }
+        ]
+}
+]:
